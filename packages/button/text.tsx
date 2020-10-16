43bdaaa633fbaaabde0085cd97b70368cd1d8e2d
xrk <xrkffgg@gmail.com>
2020-10-16 10:43:42 +0800

    feat: add btn text & chore (#1)

---
+++ packages/button/text.tsx
@@ -0,0 +1,41 @@
+/**
+ * desc: 多彩的文字增加
+ */
+
+import React from 'react';
+import { Button, Space } from 'antd';
+import './button.less';
+import './text.less';
+
+export default () => (
+  <Space size="large">
+    <Button
+      size="large"
+      type="text"
+      className="antd-colorful btn-up btn-text-spring"
+    >
+      春天的风
+    </Button>
+    <Button
+      size="large"
+      type="text"
+      className="antd-colorful btn-up btn-text-summer"
+    >
+      夏天的雨
+    </Button>
+    <Button
+      size="large"
+      type="text"
+      className="antd-colorful btn-up btn-text-autumn"
+    >
+      秋天的月
+    </Button>
+    <Button
+      size="large"
+      type="text"
+      className="antd-colorful btn-up btn-text-winter"
+    >
+      冬天的雪
+    </Button>
+  </Space>
+);
